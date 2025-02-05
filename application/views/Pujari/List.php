836df04e967d1dea6f907dbf8c36d137ef2a1d75
Rushikesh-Thomre <er.rushikeshthomare@gmail.com>
2025-02-05 12:29:44 +0530

    Add Some changes insde of that file List.php,PujaForm.php,PujariReg.php, RateChart.php,RecentRequest.php

--- application/views/Pujari/List.php
+++ application/views/Pujari/List.php
@@ -6,6 +6,8 @@
     <title>Puja List</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
     <script src="https://kit.fontawesome.com/a076d05399.js" crossorigin="anonymous"></script>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons/font/bootstrap-icons.css">
+    
     <style>
         body {
             background-color: #f8f9fa;
